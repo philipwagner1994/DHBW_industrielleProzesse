9277ffee68f408c2b7c41295e9275ece9c534b6b
JanVoegeli <s141995@student.dhbw-mannheim.de>
2017-03-09 11:39:23 +0100

    Update
    Nun für 17-dimensionen geeignet

--- Reichwald2/src/PSOProcess.java
+++ Reichwald2/src/PSOProcess.java
@@ -50,19 +50,23 @@ public class PSOProcess implements PSOConstants {
 				
 				// step 3 - update velocity
 				double[] newVel = new double[PROBLEM_DIMENSION];
-				newVel[0] = (w * p.getVelocity().getPos()[0]) + 
-							(r1 * C1) * (pBestLocation.get(i).getLoc()[0] - p.getLocation().getLoc()[0]) +
-							(r2 * C2) * (gBestLocation.getLoc()[0] - p.getLocation().getLoc()[0]);
-				newVel[1] = (w * p.getVelocity().getPos()[1]) + 
+				for(int j=0;j<PROBLEM_DIMENSION;j++){
+				newVel[j] = (w * p.getVelocity().getPos()[j]) + 
+							(r1 * C1) * (pBestLocation.get(i).getLoc()[j] - p.getLocation().getLoc()[j]) +
+							(r2 * C2) * (gBestLocation.getLoc()[j] - p.getLocation().getLoc()[j]);
+				}
+				/*newVel[1] = (w * p.getVelocity().getPos()[1]) + 
 							(r1 * C1) * (pBestLocation.get(i).getLoc()[1] - p.getLocation().getLoc()[1]) +
-							(r2 * C2) * (gBestLocation.getLoc()[1] - p.getLocation().getLoc()[1]);
+							(r2 * C2) * (gBestLocation.getLoc()[1] - p.getLocation().getLoc()[1]);*/
 				Velocity vel = new Velocity(newVel);
 				p.setVelocity(vel);
 				
 				// step 4 - update location
 				double[] newLoc = new double[PROBLEM_DIMENSION];
-				newLoc[0] = p.getLocation().getLoc()[0] + newVel[0];
-				newLoc[1] = p.getLocation().getLoc()[1] + newVel[1];
+				for(int j=0;j<PROBLEM_DIMENSION;j++){
+				newLoc[j] = p.getLocation().getLoc()[j] + newVel[j];
+				}
+				//newLoc[1] = p.getLocation().getLoc()[1] + newVel[1];
 				Location loc = new Location(newLoc);
 				p.setLocation(loc);
 			}
@@ -70,18 +74,23 @@ public class PSOProcess implements PSOConstants {
 			err = ProblemSet.evaluate(gBestLocation) - 0; // minimizing the functions means it's getting closer to 0
 			
 			
-			System.out.println("ITERATION " + t + ": ");
-			System.out.println("     Best X: " + gBestLocation.getLoc()[0]);
-			System.out.println("     Best Y: " + gBestLocation.getLoc()[1]);
-			System.out.println("     Value: " + ProblemSet.evaluate(gBestLocation));
+			/*System.out.println("ITERATION " + t + ": ");
+			for(int j=0;j<PROBLEM_DIMENSION;j++){
+			System.out.println("     Best X"+j+": " + gBestLocation.getLoc()[j]);
+			}
+			//System.out.println("     Best Y: " + gBestLocation.getLoc()[1]);
+			System.out.println("     Value: " + ProblemSet.evaluate(gBestLocation));*/
 			
 			t++;
 			updateFitnessList();
 		}
 		
 		System.out.println("\nSolution found at iteration " + (t - 1) + ", the solutions is:");
-		System.out.println("     Best X: " + gBestLocation.getLoc()[0]);
-		System.out.println("     Best Y: " + gBestLocation.getLoc()[1]);
+		for(int j=0;j<PROBLEM_DIMENSION;j++){
+		System.out.println("     Best X"+j+": " + gBestLocation.getLoc()[j]);
+		}
+		System.out.println("     Value: " + ProblemSet.evaluate(gBestLocation));
+		//System.out.println("     Best Y: " + gBestLocation.getLoc()[1]);
 	}
 	
 	public void initializeSwarm() {
@@ -91,14 +100,46 @@ public class PSOProcess implements PSOConstants {
 			
 			// randomize location inside a space defined in Problem Set
 			double[] loc = new double[PROBLEM_DIMENSION];
-			loc[0] = ProblemSet.LOC_X_LOW + generator.nextDouble() * (ProblemSet.LOC_X_HIGH - ProblemSet.LOC_X_LOW);
-			loc[1] = ProblemSet.LOC_Y_LOW + generator.nextDouble() * (ProblemSet.LOC_Y_HIGH - ProblemSet.LOC_Y_LOW);
+			loc[0] = ProblemSet.LOC_X1_LOW + generator.nextDouble() * (ProblemSet.LOC_X1_HIGH - ProblemSet.LOC_X1_LOW);
+			loc[1] = ProblemSet.LOC_X2_LOW + generator.nextDouble() * (ProblemSet.LOC_X2_HIGH - ProblemSet.LOC_X2_LOW);
+			loc[2] = ProblemSet.LOC_X3_LOW + generator.nextDouble() * (ProblemSet.LOC_X3_HIGH - ProblemSet.LOC_X3_LOW);
+			loc[3] = ProblemSet.LOC_X4_LOW + generator.nextDouble() * (ProblemSet.LOC_X4_HIGH - ProblemSet.LOC_X4_LOW);
+			loc[4] = ProblemSet.LOC_X5_LOW + generator.nextDouble() * (ProblemSet.LOC_X5_HIGH - ProblemSet.LOC_X5_LOW);
+			loc[5] = ProblemSet.LOC_X6_LOW + generator.nextDouble() * (ProblemSet.LOC_X6_HIGH - ProblemSet.LOC_X6_LOW);
+			loc[6] = ProblemSet.LOC_X7_LOW + generator.nextDouble() * (ProblemSet.LOC_X7_HIGH - ProblemSet.LOC_X7_LOW);
+			loc[7] = ProblemSet.LOC_X8_LOW + generator.nextDouble() * (ProblemSet.LOC_X8_HIGH - ProblemSet.LOC_X8_LOW);
+			loc[8] = ProblemSet.LOC_X9_LOW + generator.nextDouble() * (ProblemSet.LOC_X9_HIGH - ProblemSet.LOC_X9_LOW);
+			loc[9] = ProblemSet.LOC_X10_LOW + generator.nextDouble() * (ProblemSet.LOC_X10_HIGH - ProblemSet.LOC_X10_LOW);
+			loc[10] = ProblemSet.LOC_X11_LOW + generator.nextDouble() * (ProblemSet.LOC_X11_HIGH - ProblemSet.LOC_X11_LOW);
+			loc[11] = ProblemSet.LOC_X12_LOW + generator.nextDouble() * (ProblemSet.LOC_X12_HIGH - ProblemSet.LOC_X12_LOW);
+			loc[12] = ProblemSet.LOC_X13_LOW + generator.nextDouble() * (ProblemSet.LOC_X13_HIGH - ProblemSet.LOC_X13_LOW);
+			loc[13] = ProblemSet.LOC_X14_LOW + generator.nextDouble() * (ProblemSet.LOC_X14_HIGH - ProblemSet.LOC_X14_LOW);
+			loc[14] = ProblemSet.LOC_X15_LOW + generator.nextDouble() * (ProblemSet.LOC_X15_HIGH - ProblemSet.LOC_X15_LOW);
+			loc[15] = ProblemSet.LOC_X16_LOW + generator.nextDouble() * (ProblemSet.LOC_X16_HIGH - ProblemSet.LOC_X16_LOW);
+			loc[16] = ProblemSet.LOC_X17_LOW + generator.nextDouble() * (ProblemSet.LOC_X17_HIGH - ProblemSet.LOC_X17_LOW);
 			Location location = new Location(loc);
 			
 			// randomize velocity in the range defined in Problem Set
 			double[] vel = new double[PROBLEM_DIMENSION];
 			vel[0] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
 			vel[1] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[2] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[3] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[4] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[5] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[6] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[7] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[8] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[9] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[10] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[11] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[12] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[13] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[14] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[15] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			vel[16] = ProblemSet.VEL_LOW + generator.nextDouble() * (ProblemSet.VEL_HIGH - ProblemSet.VEL_LOW);
+			
+			
 			Velocity velocity = new Velocity(vel);
 			
 			p.setLocation(location);
--- Reichwald2/src/ProblemSet.java
+++ Reichwald2/src/ProblemSet.java
@@ -1,8 +1,38 @@
 public class ProblemSet {
-	public static final double LOC_X_LOW = 1;
-	public static final double LOC_X_HIGH = 4;
-	public static final double LOC_Y_LOW = -1;
-	public static final double LOC_Y_HIGH = 1;
+	public static final double LOC_X1_LOW = 0;
+	public static final double LOC_X1_HIGH = 1;
+	public static final double LOC_X2_LOW = 0;
+	public static final double LOC_X2_HIGH = 1;
+	public static final double LOC_X3_LOW = 0;
+	public static final double LOC_X3_HIGH = 1;
+	public static final double LOC_X4_LOW = 0;
+	public static final double LOC_X4_HIGH = 1;
+	public static final double LOC_X5_LOW = 0;
+	public static final double LOC_X5_HIGH = 1;
+	public static final double LOC_X6_LOW = 0;
+	public static final double LOC_X6_HIGH = 1;
+	public static final double LOC_X7_LOW = 0;
+	public static final double LOC_X7_HIGH = 1;
+	public static final double LOC_X8_LOW = 0;
+	public static final double LOC_X8_HIGH = 1;
+	public static final double LOC_X9_LOW = 0;
+	public static final double LOC_X9_HIGH = 1;
+	public static final double LOC_X10_LOW = 0;
+	public static final double LOC_X10_HIGH = 1;
+	public static final double LOC_X11_LOW = 0;
+	public static final double LOC_X11_HIGH = 1;
+	public static final double LOC_X12_LOW = 0;
+	public static final double LOC_X12_HIGH = 1;
+	public static final double LOC_X13_LOW = 0;
+	public static final double LOC_X13_HIGH = 1;
+	public static final double LOC_X14_LOW = 0;
+	public static final double LOC_X14_HIGH = 1;
+	public static final double LOC_X15_LOW = 0;
+	public static final double LOC_X15_HIGH = 1;
+	public static final double LOC_X16_LOW = 0;
+	public static final double LOC_X16_HIGH = 1;
+	public static final double LOC_X17_LOW = 0;
+	public static final double LOC_X17_HIGH = 1;
 	public static final double VEL_LOW = -1;
 	public static final double VEL_HIGH = 1;
 	
@@ -10,14 +40,50 @@ public class ProblemSet {
 	                                                  // but the number of iteration is increased
 	
 	public static double evaluate(Location location) {
-		double result = 0;
+		/*double result = 0;
 		double x = location.getLoc()[0]; // the "x" part of the location
 		double y = location.getLoc()[1]; // the "y" part of the location
 		
 		result = Math.pow(2.8125 - x + x * Math.pow(y, 4), 2) + 
 				Math.pow(2.25 - x + x * Math.pow(y, 2), 2) + 
 				Math.pow(1.5 - x + x * y, 2);
+		*/
+		double result = 0;
+		double x1 = location.getLoc()[0]; 
+		double x2 = location.getLoc()[1];
+		double x3 = location.getLoc()[2]; 
+		double x4 = location.getLoc()[3];
+		double x5 = location.getLoc()[4];
+		double x6 = location.getLoc()[5];
+		double x7 = location.getLoc()[6];
+		double x8 = location.getLoc()[7];
+		double x9 = location.getLoc()[8];
+		double x10 = location.getLoc()[9];
+		double x11 = location.getLoc()[10];
+		double x12 = location.getLoc()[11];
+		double x13 = location.getLoc()[12];
+		double x14 = location.getLoc()[13];
+		double x15 = location.getLoc()[14];
+		double x16 = location.getLoc()[15];
+		double x17 = location.getLoc()[16];
 		
+		result = Math.pow(x1,2)
+				+ Math.pow(x2,2)
+				+ Math.pow(x3,2)
+				+ Math.pow(x4,2)
+				+ Math.pow(x5,2)
+				+ Math.pow(x6,2)
+				+ Math.pow(x7,2)
+				+ Math.pow(x8,2)
+				+ Math.pow(x9,2)
+				+ Math.pow(x10,2)
+				+ Math.pow(x11,2)
+				+ Math.pow(x12,2)
+				+ Math.pow(x13,2)
+				+ Math.pow(x14,2)
+				+ Math.pow(x15,2)
+				+ Math.pow(x16,2)
+				+ Math.pow(x17,2);
 		return result;
 	}
 }
