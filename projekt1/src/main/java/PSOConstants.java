84e8a636a4fa5be01eb44f0a017be2bc748cdc81
MireillePuschmann <Mireille.Puschmann@gmx.de>
2017-04-01 19:40:25 +0200

    Änderungen. Kleinster Wert bei 10

--- projekt1/src/main/java/PSOConstants.java
+++ projekt1/src/main/java/PSOConstants.java
@@ -1,24 +1,26 @@
 public interface PSOConstants {
-	int SWARM_SIZE = 1000;
-	int MAX_ITERATION = 50;
+	int SWARM_SIZE = 500;
+	int MAX_ITERATION = 1000;
 	int PROBLEM_DIMENSION = 17;
-	double C1 = 1.0;
-	double C2 = 1.0;
-	/*double C3 = 2.0;
-	double C4 = 2.0;
-	double C5 = 2.0;
-	double C6 = 2.0;
-	double C7 = 2.0;
-	double C8 = 2.0;
-	double C9 = 2.0;
-	double C10 = 2.0;
-	double C11 = 2.0;
-	double C12 = 2.0;
-	double C13 = 2.0;
-	double C14 = 2.0;
-	double C15 = 2.0;
-	double C16 = 2.0;
-	double C17 = 2.0;*/
-	double W_UPPERBOUND = 0.35;
+	double C1 = 0.001;
+	double C2 = 2.0;
+	/*
+	 * double C3 = 2.0;
+	 * double C4 = 2.0;
+	 * double C5 = 2.0;
+	 * double C6 = 2.0;
+	 * double C7 = 2.0;
+	 * double C8 = 2.0;
+	 * double C9 = 2.0;
+	 * double C10 = 2.0;
+	 * double C11 = 2.0;
+	 * double C12 = 2.0;
+	 * double C13 = 2.0;
+	 * double C14 = 2.0;
+	 * double C15 = 2.0;
+	 * double C16 = 2.0;
+	 * double C17 = 2.0;
+	 */
+	double W_UPPERBOUND = 0.3827;
 	double W_LOWERBOUND = 0;
 }
