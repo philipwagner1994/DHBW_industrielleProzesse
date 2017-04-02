52c34ea9b337cd26dd664b502aeacf85dce2acb5
MireillePuschmann <Mireille.Puschmann@gmx.de>
2017-04-02 17:46:48 +0200

    Value is nearly 0, made few changes

--- projekt1/src/main/java/PSOConstants.java
+++ projekt1/src/main/java/PSOConstants.java
@@ -1,6 +1,6 @@
 public interface PSOConstants {
 	int SWARM_SIZE = 500;
-	int MAX_ITERATION = 1000;
+	int MAX_ITERATION = 100000;
 	int PROBLEM_DIMENSION = 17;
 	double C1 = 0.001;
 	double C2 = 2.0;
