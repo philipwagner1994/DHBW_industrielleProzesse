ccbc13c8aaf8c8c8aa367f51c81cab030303e717
MireillePuschmann <Mireille.Puschmann@gmx.de>
2017-04-09 18:06:23 +0200

    Changes for function 3, minimum = 2,71..

--- projekt1/src/main/java/PSOConstants.java
+++ projekt1/src/main/java/PSOConstants.java
@@ -2,25 +2,9 @@ public interface PSOConstants {
 	int SWARM_SIZE = 500;
 	int MAX_ITERATION = 100000;
 	int PROBLEM_DIMENSION = 17;
-	double C1 = 0.001;
-	double C2 = 2.0;
-	/*
-	 * double C3 = 2.0;
-	 * double C4 = 2.0;
-	 * double C5 = 2.0;
-	 * double C6 = 2.0;
-	 * double C7 = 2.0;
-	 * double C8 = 2.0;
-	 * double C9 = 2.0;
-	 * double C10 = 2.0;
-	 * double C11 = 2.0;
-	 * double C12 = 2.0;
-	 * double C13 = 2.0;
-	 * double C14 = 2.0;
-	 * double C15 = 2.0;
-	 * double C16 = 2.0;
-	 * double C17 = 2.0;
-	 */
-	double W_UPPERBOUND = 0.3827;
-	double W_LOWERBOUND = 0;
+	double C1 = 0.7005;
+	double C2 = 1.0101;
+
+	double W_UPPERBOUND = 0.5005;
+	double W_LOWERBOUND = 0.005;
 }
