8bb20b3f18734a299d567c07cce5aa4964ef0a6f
MireillePuschmann <Mireille.Puschmann@gmx.de>
2017-04-17 15:53:37 +0200

    FType 4 minimum -637

--- projekt1/src/main/java/PSOConstants.java
+++ projekt1/src/main/java/PSOConstants.java
@@ -2,9 +2,9 @@ public interface PSOConstants {
 	int SWARM_SIZE = 500;
 	int MAX_ITERATION = 100000;
 	int PROBLEM_DIMENSION = 17;
-	double C1 = 0.7005;
-	double C2 = 1.0101;
+	double C1 = 0.48;
+	double C2 = 0.7715;
 
-	double W_UPPERBOUND = 0.5005;
+	double W_UPPERBOUND = 0.79;
 	double W_LOWERBOUND = 0.005;
 }
